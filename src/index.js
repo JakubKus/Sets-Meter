7e8dda721c154450ba5a349b4801053b76f045f1
Kuba <kuba.mistrz@o2.pl>
2018-08-18 21:57:32 +0200

    implemented adding new exercises

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
+import './style/index.css'
+import ExerciseEditor from './ExerciseEditor';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showExerciseEditor: true,
+      enteredExercise: "",
+      enteredSetsNum: 1,
+      exercisesList: []
     }
   }
+
+  enterExercise = (input) => {
+    const inputVal = input.target.value;
+    this.setState({enteredExercise: inputVal});
+  };
+
+  enterSetsNum = (number) => {
+    const checkedNumber = number.target.value;
+    this.setState({enteredSetsNum: checkedNumber});
+  };
+
+  addExercise = () => {
+    const newExercise = this.state.exercisesList;
+    newExercise.push({
+      exercise: this.state.enteredExercise,
+      sets: this.state.enteredSetsNum
+    });
+    this.setState({
+      exercisesList: newExercise,
+      enteredExercise: "",
+      enteredSetsNum: 1
+    });
+    document.querySelector(".exerciseEditor input").value = "";
+  };
+
+  hideExerciseEditor = () => {
+    this.setState({showExerciseEditor: false})
+  };
 
   render() {
     return (
       <div>
+        <ExerciseEditor
+          enterExercise={this.enterExercise}
+          enterSetsNum={this.enterSetsNum}
+          addExercise={this.addExercise}
+          hideExerciseEditor={this.hideExerciseEditor}
+        />
       </div>
     )
   }
