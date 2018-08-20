52098cbeff8995f16ad73a7609062431e59f25a8
Kuba <kuba.mistrz@o2.pl>
2018-08-20 21:04:34 +0200

    added icons instead of text

--- src/Sets.js
+++ src/Sets.js
@@ -5,15 +5,25 @@ export default class Sets extends React.Component {
     const sets = this.props.setsList.map((set, index) => {
       return (
         <div key={index}>
-          <button className="subtractSet">-</button>
+          <button className="subtractSet">
+            <img src="subtract.svg" alt="subtract"/>
+          </button>
           <div className="setsMeter">
             <span className="actualSet">1</span>
             <span>/{set.setsNum}</span>
           </div>
-          <button className="addSet">+</button>
+          <button className="addSet">
+            <img src="add.svg" alt="add"/>
+          </button>
           <span className="exerciseName">{set.exercise}</span>
-          <button className="setEdit">Edit</button>
-          <button className="setDone">Done</button>
+          <button className="setEdit">
+            <img src="edit.svg" alt="edit"/>
+          </button>
+          <button className="setDone"
+                  onClick={() => {this.props.doneSet(index)}}
+          >
+            <img src="done.svg" alt="done"/>
+          </button>
         </div>
       )
     });
