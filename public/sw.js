4900602522aef26715ff8c1934ac05afce634781
Kuba <kuba.mistrz@o2.pl>
2018-11-15 18:16:17 +0100

    implemented street workout mode (exercises circuit)

--- public/sw.js
+++ public/sw.js
@@ -3,20 +3,34 @@ self.onnotificationclick = (event) => {
   const { data } = event.notification;
 
   if (data.setsList.length > 0) {
-    const { setsList, breakTime } = data;
+    const { setsList, breakTime, notifyMode } = data;
     let firstSet = false;
+    let setsListLength = setsList.length;
 
     if (setsList[0].setsNum) {
       firstSet = true;
-      const setsListLength = setsList.length;
-      for (let i = 0; i < setsListLength; i += 1) {
-        const actualSet = setsList.shift();
-        for (let j = 0; j < actualSet.setsNum; j += 1) {
+      if (notifyMode === 'gym') {
+        for (let i = 0; i < setsListLength; i += 1) {
+          const actualSet = setsList.shift();
+          for (let j = 0; j < actualSet.setsNum; j += 1) {
+            setsList.push(actualSet.exercise);
+          }
+        }
+      } else if (notifyMode === 'sw') {
+        let volume = 0;
+        setsList.forEach((set) => { volume += +set.setsNum; });
+        for (let i = 0; i < volume; i += 1) {
+          const actualSet = setsList.shift();
+          if (actualSet.setsNum > 1) {
+            actualSet.setsNum -= 1;
+            setsList.splice(setsListLength - 1, 0, actualSet);
+          } else {
+            setsListLength -= 1;
+          }
           setsList.push(actualSet.exercise);
         }
       }
     }
-
     const set = setsList.shift();
     const options = {
       vibrate: [100, 50, 100],
