3c950279d6baeff2dc58a9dd630b90b0bd862640
Kuba <kuba.mistrz@o2.pl>
2018-11-16 13:50:19 +0100

    tested addSetButton

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import App from '../App';
 
 afterEach(cleanup);
 
 test('renders without errors', () => {
   const { container } = render(<App />);
+
   expect(container).toBeTruthy();
+});
+
+test('opens SetEditor by clicking on the addSetButton', () => {
+  const { getByAltText, container } = render(<App />);
+  const addSetButton = getByAltText('add');
+
+  fireEvent.click(addSetButton);
+
+  expect(container.querySelector('.setEditor').className).toBe('setEditor on');
 });
