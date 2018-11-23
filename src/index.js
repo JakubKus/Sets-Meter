29ec1956f69328b1cc2100d171031395cb4e07ae
Kuba <kuba.mistrz@o2.pl>
2018-11-24 00:08:32 +0100

    fixed App import path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WebFont from 'webfontloader';
 import ReactGA from 'react-ga';
-import App from './App';
+import App from './components/App/App';
 
 WebFont.load({
   google: {
