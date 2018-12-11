85009490b0dae85deba6e60a4d8dc96fa384fd3f
Kuba <kuba.mistrz@o2.pl>
2018-12-11 22:56:29 +0100

    renamed Settings to NotifySettings...
    * changed mode text to barbells icon
    * added clock-start icon with on and off options
    * extended instructions and moved them outside

---
+++ src/components/NotifySettings/NotifySettings.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotifySettings = ({
+  showNotifySettings,
+  notifyMode,
+  changeNotifyMode,
+  showNotifyInstr,
+  toggleNotifyInstr,
+  notifyStatus,
+  startNotifyTimer,
+  stopNotifyTimer,
+  gaEvent,
+}) => {
+  const notifySettings = showNotifySettings
+    ? 'notifySettings'
+    : 'notifySettings off';
+
+  return (
+    <>
+      <div className={notifySettings}>
+        <img
+          src="./info.svg"
+          onClick={() => {
+            toggleNotifyInstr();
+            gaEvent('NotifySettings', 'Clicked on the info icon');
+          }}
+          className="info"
+          alt="info"
+        />
+        <div className="settings">
+          <div className="mode">
+            <img src="./barbells.svg" alt="barbells" />
+            <button
+              onClick={() => {
+                gaEvent('NotifySettings', 'Switched notify mode to sw');
+                changeNotifyMode('sw');
+              }}
+              className={notifyMode === 'sw' ? 'sw checked' : 'sw'}
+            >
+              {'Sw'}
+            </button>
+            <button
+              onClick={() => {
+                gaEvent('NotifySettings', 'Switched notify mode to gym');
+                changeNotifyMode('gym');
+              }}
+              className={notifyMode === 'gym' ? 'gym checked' : 'gym'}
+            >
+              {'Gym'}
+            </button>
+          </div>
+          <div className="notifyTimer">
+            <img src="./clock-start.svg" alt="clock start" />
+            <button
+              onClick={() => {
+                gaEvent('NotifySettings', 'Started notify timer');
+                startNotifyTimer();
+              }}
+              className={notifyStatus ? 'on checked' : 'on'}
+            >
+              {'On'}
+            </button>
+            <button
+              onClick={() => {
+                gaEvent('NotifySettings', 'Stopped notify timer');
+                stopNotifyTimer();
+              }}
+              className={notifyStatus ? 'off' : 'off checked'}
+            >
+              {'Off'}
+            </button>
+          </div>
+        </div>
+      </div>
+      <article
+        onClick={() => {
+          gaEvent('NotifySettings', 'Closed instructions by clicking on them');
+          toggleNotifyInstr();
+        }}
+        className={showNotifyInstr && showNotifySettings ? 'instr' : 'instr off'}
+      >
+        {'Select training mode to change order of notifications'}
+        <hr />
+        {'Sw: exercise after exercise in the circuit'}
+        <hr />
+        {'Gym: set after set, exercise after exercise'}
+        <hr />
+        {'Click the bell to receive a notification when the break ends'}
+      </article>
+    </>
+  );
+};
+
+NotifySettings.propTypes = {
+  showNotifySettings: PropTypes.bool.isRequired,
+  notifyMode: PropTypes.string.isRequired,
+  changeNotifyMode: PropTypes.func.isRequired,
+  showNotifyInstr: PropTypes.bool.isRequired,
+  toggleNotifyInstr: PropTypes.func.isRequired,
+  notifyStatus: PropTypes.bool.isRequired,
+  startNotifyTimer: PropTypes.func.isRequired,
+  stopNotifyTimer: PropTypes.func.isRequired,
+  gaEvent: PropTypes.func.isRequired,
+};
+
+export default NotifySettings;
